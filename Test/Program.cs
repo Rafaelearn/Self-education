892bedca2afdc6ac5b6b4f972b614735b328f9f5
Рафаэль Халилулин <RFKhalilulin@stud.kpfu.ru>
2021-09-24 09:04:35 +0300

    1

---
+++ Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+         enum Mounths
+        {
+            Январь,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine((Mounths)1);
+
+
+        }
+    }
+}
